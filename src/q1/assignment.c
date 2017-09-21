a56e8976c9f8b791e8c7714f90c6a1d86f562215
ANDRE  GODINEZ <15460718@IT.NUIGALWAY.IE>
2017-09-21 14:57:39 +0100

    fixed q1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -2,5 +2,20 @@
 
 int main(int arg, char* argc[]){
   printf("Hello assignment1.\n");
+
+  int Int;
+  int* intPointer;
+  long Long;
+  double* doublePointer;
+  char** charDoublePointer;
+
+  printf("Size of int = %d \n", sizeof(Int));
+  printf("Size of int = %d \n", sizeof(intPointer));
+  printf("Size of int = %ld \n", sizeof(Long));
+  printf("Size of int = %d \n", sizeof(doublePointer));
+  printf("Size of int = %d \n", sizeof(charDoublePointer));
+
+
+
   return 0;
 }
